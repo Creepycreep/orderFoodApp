330c0cd3fc770403792dfe2a9620362e07c06f4d
Elizaveta Shevtsova <111qw95@gmail.com>
2023-12-03 15:08:42 +0300

    add cart counter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 
 import LocalPizzaRoundedIcon from '@mui/icons-material/LocalPizzaRounded';
 import SearchIcon from '@mui/icons-material/Search';
@@ -8,9 +8,17 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Container from '../../view/Container/Container';
 import Cart from '../Cart/Cart';
 import Button from '../../view/Button/Button';
+import Order from '../../context/CartContext';
 
 const Header = () => {
   const [isCartActive, setIsCartActive] = useState(false);
+  const [orderAmount, setOrderAmount] = useState(0);
+
+  const { orderList } = useContext(Order);
+
+  useEffect(() => {
+    setOrderAmount(orderList.length)
+  }, [orderList])
 
   const onCartTriggerHandler = () => {
     setIsCartActive(prev => !prev)
@@ -35,14 +43,23 @@ const Header = () => {
         <Button
           type='icon'
           onClick={onCartTriggerHandler}
-          color='text-green-100 hover:text-green-300'
+          color='text-green-100 hover:text-green-300 relative'
         >
           {isCartActive ? <CloseIcon /> : <ShoppingCartIcon />}
+          {orderAmount ? <AmountView amount={orderAmount} /> : null}
         </Button>
       </Container>
 
       <Cart isActive={isCartActive} onCartTriggerHandler={setIsCartActive} />
     </header>
+  )
+}
+
+const AmountView = ({ amount }: { amount: number }) => {
+  return (
+    <div className='absolute top-0 right-0 flex items-center justify-center bg-white rounded-full text-green-600 text-xs leading-none p-1 w-[20px] h-[20px]'>
+      {amount}
+    </div>
   )
 }
 
